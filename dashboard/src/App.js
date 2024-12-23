0c1578e6c22822e760cc28769e526dd34b53e6d7
Swayam Shah <swayamshah66@gmail.com>
2024-12-23 18:21:04 +0530

    animated

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import logo from "./assets/logo.png"; // Ensure the path is correct
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 500); // Duration of the fade-out animation
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
+    <div className={`App ${isLoading ? "fade-out" : "fade-in"}`}>
       <header className="App-header">
         <a href="/">
-          {" "}
-          {/* Use anchor tag for refreshing the page */}
           <img src={logo} alt="Logo" className="App-logo" />
         </a>
       </header>
