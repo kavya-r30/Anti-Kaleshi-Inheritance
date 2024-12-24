7f5c6666e5fd7df6d5bd6c85a6c5e65b234f1882
Swayam Shah <swayamshah66@gmail.com>
2024-12-24 18:12:56 +0530

    added animation files

---
+++ dashboard/src/components/Animation.js
@@ -0,0 +1,93 @@
+import React from "react";
+import "./Animation.scss"; // Import the CSS file
+
+const Animation = () => {
+  return (
+    <div className="container">
+      <div className="box">
+        <span style={{ "--i": 1 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 2 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 3 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 4 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 5 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 6 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 7 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 8 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 9 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 10 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 11 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 12 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 13 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 14 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 15 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+        <span style={{ "--i": 16 }}>
+          <i>COMPETE</i>
+          ANALYSE
+          <i>IMPROVE</i>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Animation;
