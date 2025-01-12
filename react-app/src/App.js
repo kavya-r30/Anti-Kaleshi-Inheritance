8f1da5c175575c70f15616ea8fd8391292227a0f
Kavya Rambhia <161142013+kavya-r30@users.noreply.github.com>
2025-01-12 17:32:30 +0530

    Link Dashboard

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ContestCalendar from './components/ContestTracker';
 import LandingPage from './components/Landing';
 import Navbar from './components/Navbar';
+import { Dashboard } from './components/Dashboard';
 // import LoginPage from './components/login';
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
           <Route path='/contest-tracker' element={<ContestCalendar />} />
           <Route path='/chatbot' element={<h1>chatbot</h1>} />
           <Route path='/dicussion' element={<h1>dicussion</h1>} />
-          <Route path='/dashboard' element={<h1>dashboard</h1>} />
+          <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/login' element={<h1>Auth0 login</h1>} />
           <Route path='*' element={<h1>404</h1>} />
         </Routes>
