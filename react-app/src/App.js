d738285365f88d053e804492be020c975571a2cb
Kavya Rambhia <161142013+kavya-r30@users.noreply.github.com>
2025-01-19 01:00:55 +0530

    Added dashboard in app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import ContestCalendar from './components/ContestTracker';
+import ContestTracker from './components/ContestTracker';
 import LandingPage from './components/Landing';
 import Navbar from './components/Navbar';
+import ProfilePage from './components/Profile';
 import { Dashboard } from './components/Dashboard';
 // import LoginPage from './components/login';
 
@@ -14,11 +15,12 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<LandingPage />} />
-          <Route path='/contest-tracker' element={<ContestCalendar />} />
+          <Route path='/contest-tracker' element={<ContestTracker />} />
           <Route path='/chatbot' element={<h1>chatbot</h1>} />
           <Route path='/dicussion' element={<h1>dicussion</h1>} />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/login' element={<h1>Auth0 login</h1>} />
+          <Route path='/profile' element={<ProfilePage />} />
           <Route path='*' element={<h1>404</h1>} />
         </Routes>
       </Router>
