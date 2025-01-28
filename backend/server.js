0a7b11b41fefbf7fee09e8fce21461f5bba8f6f7
Kavya Rambhia <161142013+kavya-r30@users.noreply.github.com>
2025-01-29 00:21:41 +0530

    Vite cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true })); 
+app.use(cors({ origin: 'http://localhost:5173', credentials: true })); 
 app.use(express.static('public'));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
