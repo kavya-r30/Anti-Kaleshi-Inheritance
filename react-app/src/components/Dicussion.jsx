9bae02e262c629c8845bc8ff640f6b93141916e3
Kavya Rambhia <161142013+kavya-r30@users.noreply.github.com>
2025-02-02 19:21:56 +0530

    env

--- react-app/src/components/Dicussion.jsx
+++ react-app/src/components/Dicussion.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5001/comm";
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+const API_URL = `${backendUrl}/comm`;
 
 const CommunityDiscussions = () => {
   const [threads, setThreads] = useState([]);
