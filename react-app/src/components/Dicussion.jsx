6e46a2175d3d0e97b0c88f06fd8a60047943004b
Swayam Shah <swayamshah66@gmail.com>
2025-02-02 15:38:49 +0530

    discussion served merged into main server

--- react-app/src/components/Dicussion.jsx
+++ react-app/src/components/Dicussion.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/comm";
+const API_URL = "http://localhost:5001/comm";
 
 const CommunityDiscussions = () => {
   const [threads, setThreads] = useState([]);
